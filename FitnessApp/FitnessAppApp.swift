ca426c4dda83d54b836f0c333d25dda4f4a682db
abhay <abhay.chatur>
2025-11-29 09:19:15 -0500

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by abhay chaturvedi on 11/29/25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitnessAppTests/FitnessAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppTests.swift
+//  FitnessAppTests
+//
+//  Created by abhay chaturvedi on 11/29/25.
+//
+
+import Testing
+@testable import FitnessApp
+
+struct FitnessAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
